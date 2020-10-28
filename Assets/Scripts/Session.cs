7545b8a08258b5b5a0cac48cc832f36706fdd9ff
Fred Duarte <gestion_atlassian@inteligogroup.com>
2020-10-28 01:56:40 -0500

    Video Juego de Firebase y Unity

---
+++ Assets/Scripts/Session.cs
@@ -0,0 +1,27 @@
+﻿using Firebase.Auth;
+
+public static class Session
+{
+
+    private static FirebaseUser user;
+    private static FirebaseAuth loginAuth;
+
+    public static void SetUser(FirebaseUser currentUser)
+    {
+        user = currentUser;
+    }
+
+    public static FirebaseUser GetUser()
+    {
+        return user;
+    }
+    public static void SetAuth(FirebaseAuth currentLoginAuthr)
+    {
+        loginAuth = currentLoginAuthr;
+    }
+
+    public static FirebaseAuth GetAuthr()
+    {
+        return loginAuth;
+    }
+}
